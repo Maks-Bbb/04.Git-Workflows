cfd163ef646cad452529ca148ff9e9917009c942
Maksim Bogdanovich <maksbogdanovichh@gmail.com>
2024-10-27 21:14:29 +0300

    Add Test.yml

--- Test.py
+++ Test.py
@@ -4,10 +4,10 @@ class calcTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(4,7), 11)
     def test_subtract(self):
-        self.assertEqual(calc.sub(10,5), 5)
+        self.assertEqual(calc.sub(10,5), 7)
     def test_multiply(self):
         self.assertEqual(calc.mult(3,7), 21)
     def test_divide(self):
-        self.assertEqual(calc.div(10,2), 5)
+        self.assertEqual(calc.div(10,2), 3)
 if __name__ == "__main__":
     unittest.main()
--- calc.py
+++ calc.py
@@ -1,9 +1,10 @@
+import math
 class calc:
- def add(self,a,b):
+ def add(a,b):
     return a+b
- def sub(self,a,b):
+ def sub(a,b):
     return a-b
- def mult(self,a,b):
+ def mult(a,b):
      return a*b
- def div(self,a,b):
+ def div(a,b):
      return a/b
